9dffd1726cf619deebf77263dcaa9ea41c55b2d2
Anwesh43 <anweshthecool0@gmail.com>
2021-06-04 03:19:50 +0530

    adding BRTMNode class

--- index.ts
+++ index.ts
@@ -139,3 +139,45 @@ class Animator {
         }
     }
 }
+
+class BRTMNode {
+    
+    next : BRTMNode 
+    prev : BRTMNode 
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < colors.length - 1) {
+            this.next = new BRTMNode(this.i + 1)
+            this.next.prev = this 
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        DrawingUtil.drawBRTMNode(context, this.i, this.state.scale)
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : BRTMNode {
+        var curr : BRTMNode = this.prev  
+        if (dir == 1) {
+            curr = this.next 
+        }
+        if (curr) {
+            return curr 
+        }
+        cb()
+        return this 
+    }
+}
